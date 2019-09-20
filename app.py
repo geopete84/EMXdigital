29f11bf9af7d010d972b130456c6e100cf338dd5
george <taskuler@gmail.com>
2019-09-19 22:00:21 -0500

    full name

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def responder():
+    data = request.get_data()
+
+    if data == 'What is your full name?':
+        return 'George Peterson'
+
     return 'OK'
 
 
 if __name__ == '__main__':
     app.run()
+
