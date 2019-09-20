2bb4673ecc2814d84ce39e4e846d711450854f7d
george <taskuler@gmail.com>
2019-09-20 02:45:37 -0500

    getting title from args

--- app.py
+++ app.py
@@ -21,43 +21,42 @@ def log_request_info():
 
 @app.route('/', methods=['GET'])
 def responder():
-    args = request.args
-
-    if args.get('Name'):
+    title = request.args[0][1]
+    if title == 'Name':
         return 'George Peterson'
 
-    if args.get('Position'):
+    if title == 'Position':
         return 'Senior Full Stack Python Engineer'
 
-    if args.get('Ping'):
+    if title == 'Ping':
         return 'OK'
 
-    if args.get('Puzzle'):
+    if title == 'Puzzle':
         # TODO: do the puzzle
         pass
 
-    if args.get('Years'):
+    if title == 'Years':
         return '9 years'
 
-    if args.get('Degree'):
+    if title == 'Degree':
         return 'Masters of Science in Electrical and Computer Engineering from Southern Illinois University Carbondale'
 
-    if args.get('Status'):
+    if title == 'Status':
         return 'US Citizen'
 
-    if args.get('Source'):
+    if title == 'Source':
         return 'https://github.com/geopete84/EMXdigital'
 
-    if args.get('Resume'):
+    if title == 'Resume':
         return 'https://docs.google.com/document/d/e/2PACX-1vTnZCilVuyER89N3c4PLPJHscAbaoRNkXISkNP4B_oDyaxQ9k4PHIw1LVmeF5dnJakdRcpez4UMcrfg/pub'
 
-    if args.get('Phone'):
+    if title == 'Phone':
         return '8152364740'
 
-    if args.get('Referrer'):
+    if title == 'Referrer':
         return 'Glassdoor'
 
-    if args.get('Email Address'):
+    if title == 'Email Address':
         return 'geopete84@gmail.com'
 
     return 'working on it'
